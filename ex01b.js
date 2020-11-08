58fad3d13b099b1d29edabc21405efbb9b36d9d2
yaakov hatam <kobieshka@gmail.com>
2020-11-08 18:21:10 +0200

    Create ex01b.js

---
+++ ex01b.js
@@ -0,0 +1,8 @@
+var tragetNumber = prompt("Number from 1 to 9 please");
+for(var i=0; i < 10; i++) {
+    if (i === tragetNumber) {
+        console.log('equal to', tragetNumber)
+    } else {
+        console.log(i)
+    }
+}
