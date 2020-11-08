c2b2dbfe0f5a16c8f13b79b87a57a50714730006
yaakov hatam <kobieshka@gmail.com>
2020-11-08 18:41:54 +0200

    Create ex04.js

---
+++ ex04.js
@@ -0,0 +1,18 @@
+
+function createUser(name) {
+    if (name == 'admin' || name == 'administrator') {
+        return 'Username not allowed!'
+    }
+
+    else if (name.indexOf('0') === 0) { // If username starts with 0
+        return name + ' (Starts with 0)';
+    } else { 
+        name
+    }
+}
+
+createUser('admin');
+createUser('0gag');
+createUser('bullish');
+// 'Username not allowed!', '0gag (Starts with 0)', bullish
+
